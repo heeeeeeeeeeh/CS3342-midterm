bd45123e2df91da630242594e683c877579ddc29
Silvestre Espinoza <90989842+heeeeeeeeeeh@users.noreply.github.com>
2026-03-12 11:56:37 -0500

    checkpoint

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 // NOTE: You may use the sample user in the data/user.json file to test your code before your hit the API. 
 // Write your code below.
-
+$(document).ready(function() {
+  $("#next-btn").click(function() {
+    fetch("https://randomuser.me/api").then((res) => {
+      return res.json()
+    }).then((json) => {
+      let data = json.results[0]
+      console.log(data)
+      let nameObj = data.name
+      let locObj = data.location
+      $("#name").text(`${nameObj.first} ${nameObj.last}`)
+      $("#email").text(`${data.email}`)
+      $("#address").text(`${locObj.city}, ${locObj.state}. ${locObj.country}`)
+      $("#age").text(`${data.dob.age}`)
+      $("#pic").attr("src", data.picture.large)
+      if (data.gender === "male") {
+        $("body").addClass("male")
+      } else {
+        $("body").addClass("female")
+      }
+    })
+  })
+})
